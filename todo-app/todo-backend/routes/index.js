e65ac7b70b9dc87ce94928b46589dcc2b90f6cd2
nirmal0001 <kdjj18322@gmail.com>
2026-08-18 22:55:08 +0530

    complet chapter 3

--- todo-app/todo-backend/routes/index.js
+++ todo-app/todo-backend/routes/index.js
@@ -1,18 +1,23 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const redis = require("../redis");
 
-const configs = require('../util/config')
+const configs = require("../util/config");
 
-let visits = 0
+let visits = 0;
 
 /* GET index data. */
-router.get('/', async (req, res) => {
-  visits++
+router.get("/", async (req, res) => {
+  visits++;
 
   res.send({
     ...configs,
-    visits
+    visits,
   });
 });
 
+router.get("/statistics", async (req, res) => {
+  const added_todos = (await redis.get("added_todos")) | 0;
+  res.send({ added_todos });
+});
 module.exports = router;
